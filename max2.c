2b2bf09df88c8726267a02a611d33498a401bf6c
marcfedorow <marcfedorow@gmail.com>
2022-04-13 16:56:12 +0300

    Do not use multiplication

--- max2.c
+++ max2.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-#define max(a, b) (((a-b)>>(sizeof(a)*8-1))&1)*b + ((((a-b)>>(sizeof(a)*8-1))&1)^1)*a
+
+#define bpick ((a - b) >> (sizeof(a) * 8 - 1))
+#define max(a, b) bpick & b | ~bpick & a
+
 int main() {
 	int a, b;
 	return scanf("%d %d", &a, &b), printf("%d\n", max(a, b)), 0;
